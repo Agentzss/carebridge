3f40b4b30f6075b7f6f7dfee64672c7503fec17b
limzhaeris <96334099+limzhaeris@users.noreply.github.com>
2026-08-27 00:37:38 +0800

    app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ SUBSIDISED_DISCOUNT = 0.30
 MIN_APPOINTMENT_DAYS = 7
 
 TIME_SLOTS = [
-    "09:00 AM", "10:00 AM", "11:00 AM",
-    "02:00 PM", "03:00 PM", "04:00 PM"
+    "09:00 AM", "09:30 AM", "10:00 AM", "10:30 AM", "11:00 AM",
+    "02:00 PM", "02:30 PM", "03:00 PM", "03:30 PM", "04:00 PM"
 ]
 
 # In-memory storage
